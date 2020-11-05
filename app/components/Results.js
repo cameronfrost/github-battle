373319c3624904fa0ed44c0e6d910501478623d4
Cameron <c@cameronfrost.com>
2020-11-05 16:23:32 -0600

    API

--- app/components/Results.js
+++ app/components/Results.js
@@ -2,6 +2,14 @@ import React from 'react'
 import { battle } from '../utils/api'
 
 export default class Results extends React.Component {
+  componentDidMount () {
+    const { playerOne, playerTwo } = this.props
+
+    battle([ playerOne, playerTwo ])
+      .then((players) => {
+        console.log('data: ', players)
+      })
+  }
   render() {
     return (
       <div>
